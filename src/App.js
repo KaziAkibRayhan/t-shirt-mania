2b9b2bc7c54c475f1c21068402e82ee4a0e4da67
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-17 21:52:23 +0600

    added simple route

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import About from "./components/About/About";
+import Home from "./components/Home/Home";
+import Orders from "./components/Orders/Orders";
+import Main from "./layouts/Main";
 function App() {
-  const router = createBrowserRouter([]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        { path: "/", element: <Home></Home> },
+        { path: "/orders", element: <Orders></Orders> },
+        { path: "/about", element: <About></About> },
+      ],
+    },
+  ]);
   return (
     <div className="App">
       <RouterProvider router={router}></RouterProvider>
