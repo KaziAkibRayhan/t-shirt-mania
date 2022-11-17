7923b82114159ecd93d569b5922f2cebddc7f8ab
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-17 23:48:24 +0600

    delete from cart in cart components

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import Cart from "../Cart/Cart";
 import TShirt from "../TShirt/TShirt";
@@ -6,15 +6,36 @@ import "./Home.css";
 
 const Home = () => {
   const tshirts = useLoaderData();
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (tshirt) => {
+    const exists = cart.find((ts) => ts._id === tshirt._id);
+    if (exists) {
+      alert("TShirt already exists");
+    } else {
+      const newCart = [...cart, tshirt];
+      setCart(newCart);
+    }
+  };
+
+  const handleRemoveItem = (tshirt) => {
+    const remaining = cart.filter((ts) => ts._id !== tshirt._id);
+    setCart(remaining);
+  };
+
   return (
     <div className="home-container">
       <div className="t-shirt-container">
         {tshirts.map((tshirt) => (
-          <TShirt key={tshirt._id} tshirt={tshirt}></TShirt>
+          <TShirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
         ))}
       </div>
       <div className="cart-container">
-        <Cart></Cart>
+        <Cart handleRemoveItem={handleRemoveItem} cart={cart}></Cart>
       </div>
     </div>
   );
